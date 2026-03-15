c899f0455f63d4c9df92ab5e36fd3d69aa9304fa
Zane <labs.zane@gmail.com>
2026-03-15 20:29:35 +0100

    Optimizing code with functions

--- src/main.py
+++ src/main.py
@@ -3,21 +3,25 @@ import time
 from machine import Pin, ADC
 
 SOIL_PIN = 18
-capteur = dht.DHT11(Pin(15))
+DHT_PIN = 15
 
+capteur = dht.DHT11(Pin(DHT_PIN))
+adc = ADC(Pin(SOIL_PIN))
 
 def read_soil():
-    adc = ADC(Pin(SOIL_PIN))
     adc.atten(ADC.ATTN_11DB)
     raw = adc.read_u16()
     moisture_pct = max(0, min(100, 100 - (raw * 100 // 65535)))
     return raw, moisture_pct
 
-
-while True:
+def read_temp():
     capteur.measure()
     temp = capteur.temperature()
     humi = capteur.humidity()
+    return temp, humi
+
+while True:
+    temp, humi = read_temp()
     raw, soil_pct = read_soil()
 
     print(f"Température : {temp}°C")
