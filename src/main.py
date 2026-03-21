10c344d32d27d27ec4db1ecb24a0258e6572cc6e
Zane <labs.zane@gmail.com>
2026-03-21 18:32:36 +0100

    Creating access point function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import dht
 import time
 from machine import Pin, ADC, I2C
+import socket
+import network
+import asyncio
 
+import time
 #SENSOR PINS
 SOIL_PIN = 18
 DHT_PIN = 16
@@ -25,7 +29,7 @@ adc = ADC(Pin(SOIL_PIN)) #adc : analog to digital converter on soil sensor
 i2c = I2C(0, sda=Pin(LIGHT_SDA), scl=Pin(LIGHT_SCL), freq=400000)
 i2c.writeto(BH1750_ADDR, bytes([BH1750_CONT_H_RES]))
 
-def read_light():
+async def read_light():
     data = i2c.readfrom(BH1750_ADDR, 2)
     return (data[0] << 8 | data[1]) / 1.2
 
@@ -41,7 +45,17 @@ def read_temp():
     humi = capteur.humidity()
     return temp, humi
 
+
+def leaunch_ap():
+    sta = network.WLAN(network.AP_IF)
+    sta.config(essid='FloraCare', password=None)
+    sta.active(True)
+
+    print('Network config:', sta.ifconfig())
+
+
 while True:
+    leaunch_ap()
     temp, humi = read_temp()
     raw, soil_pct = read_soil()
     lux = read_light()
