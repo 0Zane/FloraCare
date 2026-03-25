44d3a4a2cf346623d6065f6be4135a3144eef071
Zane <labs.zane@gmail.com>
2026-03-25 23:30:18 +0100

    Adding plant statistic handler

---
+++ src/statehandler.py
@@ -0,0 +1,41 @@
+def plantstate(planttemp,temptarget, planthum, humtarget, plantmoist,moisttarget, plantlight,lighttarget):
+
+
+    #Defining plant tolerance to avoid too strict analysis
+    TEMP_TOLERANCE = 6 
+    HUM_TOLERANCE = 15
+    MOIST_TOLERANCE = 15
+    LIGHT_TOLERANCE = 20
+
+
+    temp_state = 0
+    hum_state = 0
+    moist_state = 0
+    light_state = 0
+
+
+    if planttemp + TEMP_TOLERANCE  < temptarget:
+        temp_state = -1
+    elif planttemp - TEMP_TOLERANCE  > temptarget:
+        temp_state = 1
+
+    if planthum + HUM_TOLERANCE < humtarget:
+        hum_state = -1
+    elif planthum - HUM_TOLERANCE  > humtarget:
+        hum_state = 1
+
+    if plantmoist + MOIST_TOLERANCE < moisttarget:
+        moist_state = -1
+    elif plantmoist - MOIST_TOLERANCE  > moisttarget:
+        moist_state = 1
+
+    if plantlight + LIGHT_TOLERANCE < lighttarget:
+        light_state = -1
+    elif plantlight - LIGHT_TOLERANCE  > lighttarget:
+        light_state = 1
+    
+
+
+    #result will be in this form (tempstate, humstate, moiststate, lightstate)
+
+    return (temp_state, hum_state, moist_state, light_state)
